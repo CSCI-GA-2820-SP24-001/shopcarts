69c9ecc78a4d4df0f49654598a5dc6f983a27347
Yan <100032599+whykay-01@users.noreply.github.com>
2024-03-29 23:58:05 +0000

    chore: added loggers to the filters for items

--- service/routes.py
+++ service/routes.py
@@ -307,13 +307,19 @@ def list_items(shopcart_id):
     # See if any query filters were passed in
     product_id = request.args.get("product_id")
     quantity = request.args.get("quantity")
-    if product_id and not quantity:
-        filtered_items = Item.find_by_product_id(product_id)
-    elif quantity and not product_id:
-        filtered_items = Item.find_by_quantity(quantity)
+    if product_id and quantity is None:
+        app.logger.info("Filtering items by the product_id")
+        filtered_items = Item.find_by_product_id(int(product_id))
+    elif quantity and product_id is None:
+        app.logger.info("Filtering items by the quantity")
+        filtered_items = Item.find_by_quantity(int(quantity))
     elif product_id and quantity:
-        filtered_items = Item.find_by_product_id_and_quantity(product_id, quantity)
+        app.logger.info("Filtering items by the product_id and quantity")
+        filtered_items = Item.find_by_product_id_and_quantity(
+            int(product_id), int(quantity)
+        )
     else:
+        app.logger.info("Requesting all the items")
         filtered_items = Item.all()
 
     # Get the items for the shopcart
