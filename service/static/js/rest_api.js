184a6a2295d59df17d57e297e17cb5766bceb17c
Yan <100032599+whykay-01@users.noreply.github.com>
2024-04-15 03:40:56 +0000

    feat: added templates for the shopcart form

---
+++ service/static/js/rest_api.js
@@ -0,0 +1,260 @@
+$(function () {
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#shopcart_id").val(res.id);
+        $("#shopcart_name").val(res.name);
+        $("#shopcart_category").val(res.category);
+        if (res.available == true) {
+            $("#shopcart_available").val("true");
+        } else {
+            $("#shopcart_available").val("false");
+        }
+        $("#shopcart_gender").val(res.gender);
+        $("#shopcart_birthday").val(res.birthday);
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#shopcart_name").val("");
+        $("#shopcart_category").val("");
+        $("#shopcart_available").val("");
+        $("#shopcart_gender").val("");
+        $("#shopcart_birthday").val("");
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a Shopcart
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        let name = $("#shopcart_name").val();
+        let category = $("#shopcart_category").val();
+        let available = $("#shopcart_available").val() == "true";
+        let gender = $("#shopcart_gender").val();
+        let birthday = $("#shopcart_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/shopcarts",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function (res) {
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // Update a Shopcart
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let shopcart_id = $("#shopcart_id").val();
+        let name = $("#shopcart_name").val();
+        let category = $("#shopcart_category").val();
+        let available = $("#shopcart_available").val() == "true";
+        let gender = $("#shopcart_gender").val();
+        let birthday = $("#shopcart_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "PUT",
+            url: `/shopcarts/${shopcart_id}`,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
+
+        ajax.done(function (res) {
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a Shopcart
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let shopcart_id = $("#shopcart_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/shopcarts/${shopcart_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function (res) {
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function (res) {
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Shopcart
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        let shopcart_id = $("#shopcart_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/shopcarts/${shopcart_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function (res) {
+            clear_form_data()
+            flash_message("Shopcart has been Deleted!")
+        });
+
+        ajax.fail(function (res) {
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#shopcart_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a Shopcart
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        let name = $("#shopcart_name").val();
+        let category = $("#shopcart_category").val();
+        let available = $("#shopcart_available").val() == "true";
+
+        let queryString = ""
+
+        if (name) {
+            queryString += 'name=' + name
+        }
+        if (category) {
+            if (queryString.length > 0) {
+                queryString += '&category=' + category
+            } else {
+                queryString += 'category=' + category
+            }
+        }
+        if (available) {
+            if (queryString.length > 0) {
+                queryString += '&available=' + available
+            } else {
+                queryString += 'available=' + available
+            }
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/shopcarts?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function (res) {
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Name</th>'
+            table += '<th class="col-md-2">Category</th>'
+            table += '<th class="col-md-2">Available</th>'
+            table += '<th class="col-md-2">Gender</th>'
+            table += '<th class="col-md-2">Birthday</th>'
+            table += '</tr></thead><tbody>'
+            let firstShopcart = "";
+            for (let i = 0; i < res.length; i++) {
+                let shopcart = res[i];
+                table += `<tr id="row_${i}"><td>${shopcart.id}</td><td>${shopcart.name}</td><td>${shopcart.category}</td><td>${shopcart.available}</td><td>${shopcart.gender}</td><td>${shopcart.birthday}</td></tr>`;
+                if (i == 0) {
+                    firstShopcart = shopcart;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
+
+            // copy the first result to the form
+            if (firstShopcart != "") {
+                update_form_data(firstShopcart)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+})
